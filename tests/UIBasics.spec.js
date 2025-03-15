45a0adf38caa824bfbbf44864f310bf7017b7736
sadia <imsadiazaidi@yahoo.com>
2025-03-15 08:57:23 -0400

    This is my first commit to git

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,16 @@
+const {test} = require ('@playwright/test');
+
+test ('First Playwright test', async ({browser,page})=>
+{
+//playwright code
+//const context = await browser.newContext();
+//const page = await context.newPage();
+await page.goto("https://www.google.com/");
+});
+
+
+//
+//test.only ('Google page open',async ({page})=>
+//{
+//await page.goto("https://www.google.com/");
+//});
